6a850ca4a4a26367f78f7124fe9731c87da2cd82
Your Name <keshavsgoodlife@gmail.com>
2024-12-27 10:09:45 +0530

    git@github.com:KBB2000/shoppin-frontend.git

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Studio from "../../../components/Studio";
+import React, { useState } from "react";
+import Header from "../../../components/Hero";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Studio />
+    </>
+  );
+};
+
+export default page;
